caf6114b348df443b8f99b6883db76b44f749f8c
German Lescano <120352109+g120795@users.noreply.github.com>
2026-07-13 23:58:05 -0500

    config

--- config/views.py
+++ config/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def home(request):
     return render(request,'home.html')
-
+@login_required
 def features(request):
     return render(request, 'features.html')
 
@@ -11,19 +13,19 @@ def about_us(request):
 
 def contact(request):
     return render(request, 'contact.html')
-
+@login_required
 def product(request):
     return render(request, 'product.html')
-
+@login_required
 def staff_link(request):
     return render(request, 'staff_link.html')
-
+@login_required
 def order(request):
     return render(request, 'order.html')
-
+@login_required
 def catalog_link(request):
     return render(request, 'catalog_link.html')
-
+@login_required
 def seller_function(request):
     return render(request, 'seller_function.html')
 
